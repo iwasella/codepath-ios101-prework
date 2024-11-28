717064d9fa2366335a46e3d1f1b41c5f637c7e34
iwasella <145622813+iwasella@users.noreply.github.com>
2024-11-27 18:48:04 -0600

    Added prework files to repository

---
+++ CodePathIOS101Prework/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  CodePathIOS101Prework
+//
+//  Created by Ella Vu on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        
+        func changeColor() -> UIColor{
+
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
+    
+}
+
